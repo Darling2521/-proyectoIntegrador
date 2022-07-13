2098b8991caa733410e2cb546fa1855ce931e9a3
Alexis Pantoja <ajr.pantoja@yavirac.edu.ec>
2022-07-12 22:13:44 -0500

    accept change

--- backend-pi/src/main/java/edu/yavirac/backendpi/feature/opportunity/Opportunity.java
+++ backend-pi/src/main/java/edu/yavirac/backendpi/feature/opportunity/Opportunity.java
@@ -1,36 +1,5 @@
 package edu.yavirac.backendpi.feature.opportunity;
 
-<<<<<<< HEAD
-import java.sql.Timestamp;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.Table;
-
-import lombok.Data;
-
-@Data
-@Table("opportunities")
 public class Opportunity {
     
-    @Id
-    @Column("opportunity_id")
-    private long opportunityId;
-    @Column("prospect_id")
-    private Integer prospectId;
-    @Column("stage_id")
-    private Integer stageId;
-    @Column("vendor_id")
-    private Integer vendorId;
-    @Column("campaign_id")
-    private Integer campaignId;
-    private Integer amount;
-    private Timestamp created;
-    private Timestamp updated;
-    private boolean enable;
-
-=======
-public class Opportunity {
-    
->>>>>>> ca3ab944e700483eacec4ecf0c9a6e01a3957d33
 }
--- backend-pi/src/main/java/edu/yavirac/backendpi/feature/opportunity/OpportunityController.java
+++ backend-pi/src/main/java/edu/yavirac/backendpi/feature/opportunity/OpportunityController.java
@@ -1,21 +1,5 @@
 package edu.yavirac.backendpi.feature.opportunity;
 
-<<<<<<< HEAD
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-@RestController
-@RequestMapping("/api/opportunity")
-=======
->>>>>>> ca3ab944e700483eacec4ecf0c9a6e01a3957d33
 public class OpportunityController {
     
 }
