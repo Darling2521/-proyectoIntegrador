102b4b11d647ccc6f0162a42833e682a65350e7f
Darling Ortiz <dtv.ortiz@yavirac.edu.ec>
2022-07-12 19:14:10 -0500

    changed entidad Prospect

--- backend-pi/src/main/java/edu/yavirac/backendpi/feature/prospect/Prospect.java
+++ backend-pi/src/main/java/edu/yavirac/backendpi/feature/prospect/Prospect.java
@@ -1,5 +1,7 @@
 package edu.yavirac.backendpi.feature.prospect;
 
+import java.sql.Timestamp;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
@@ -12,5 +14,14 @@ public class Prospect {
     @Id
     @Column("prospect_id")
     private long prospectId;
+    private String name;
+    private String dni;
+    private String source;
+    private String phone;
+    private String address;
+    private String mail;
+    private Timestamp created;
+    private Timestamp updated;
+    private Boolean enable;
     
 }
